9e1a0be5d5258fa64ce92d166dc1fe9bd55f08e9
Janarthanan <janasundar1997@gmail.com>
2023-07-29 22:10:35 +0530

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,87 @@
+'use client'
+
+import { useScroll, useTransform, motion, LayoutGroup } from 'framer-motion'
 import Image from 'next/image'
+import { useRef } from 'react'
+
 
 export default function Home() {
+  const ref = useRef<HTMLSelectElement>(null)
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['end end', 'end start']
+  })
+
+  const textOpacity = useTransform(scrollYProgress, [0, 0.2], [1, 0])
+  const scale = useTransform(scrollYProgress, [0, 0.3], [1, 12])
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className='max-w-[1200px] mx-auto transition-colors overflow-clip'>
+      <motion.section ref={ref} className="flex gap-6 h-screen items-center origin-[90%_40%] transition-transform justify-center" style={{ scale }}>
+        <div className='flex-1 text-center text-gray-400/90'>
+          <motion.h1 className='text-3xl font-bold tracking-wide' style={{ opacity: textOpacity }}>Bored of traditional webpage layouts</motion.h1>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <motion.div style={{ opacity: textOpacity }} className="relative flex flex-col w-[600px] overflow-y-auto rounded-xl origin-center h-[400px] shadow border border-gray-300/20">
+          <span className='bg-gradient-to-r -z-[1] from-zinc-950 to-zinc-900 absolute inset-0' />
+          <div className='h-10 w-full border-b border-gray-100/20' />
+          <div className='h-full flex'>
+            <div className=' flex flex-col gap-4 p-4 border-r border-gray-100/20 w-20 overflow-clip'>
+              <span className='w-full h-2 rounded-full bg-gray-100/20'></span>
+              <span className='w-full h-2 rounded-full bg-gray-100/20'></span>
+              <span className='w-full h-2 rounded-full bg-gray-100/20'></span>
+              <span className='w-full h-2 rounded-full bg-gray-100/20'></span>
+              <span className='w-full h-2 rounded-full bg-gray-100/20'></span>
+            </div>
+            <div className='flex flex-col gap-4 p-4 border-r border-gray-100/20 w-full overflow-clip'>
+              <span className='w-full h-40 rounded bg-gray-100/20'></span>
+              <span className='w-full h-10 rounded bg-gray-100/20'></span>
+              <span className='space-y-2 w-full h-auto'>
+                <span className='w-full h-2 block rounded bg-gray-100/20'></span>
+                <span className='w-full h-2 block rounded bg-gray-100/20'></span>
+                <span className='w-full h-2 block rounded bg-gray-100/20'></span>
+                <span className='w-full h-2 block rounded bg-gray-100/20'></span>
+                <span className='w-1/2 h-2 block rounded bg-gray-100/20'></span>
+              </span>
+            </div>
+          </div>
+        </motion.div>
+      </motion.section>
+      <section className='flex flex-col gap-4 items-center pb-40'>
+        <div className='flex flex-col gap-4'>
+          <div className='w-[450px] flex py-2 px-4 rounded-l-xl rounded-tr-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+            <h1 className='text-sm tracking-wider'>Introducing Txtly</h1>
+          </div>
+          <div className='w-[450px] flex py-2 px-4 rounded-l-xl rounded-tr-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+            <h1 className='text-sm tracking-wider'>A messenger style personal webpage.</h1>
+          </div>
+          <div className='flex items-end gap-2'>
+            <div className='w-[450px] flex py-2 px-4 rounded-l-xl rounded-tr-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+              <h1 className='text-sm tracking-wider'>Supports all markdown syntax and also supports audio, video, spotify and more.</h1>
+            </div>
+            <img src="https://images.pexels.com/photos/5774802/pexels-photo-5774802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='w-8 h-8 rounded-full object-cover' />
+          </div>
+        </div>
+        <div className='flex items-end gap-2'>
+          <img src="https://images.pexels.com/photos/5774802/pexels-photo-5774802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='w-8 h-8 rounded-full object-cover' />
+          <div className='w-[450px] flex flex-col gap-4 py-2 px-2 rounded-r-xl rounded-tl-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+            <img src="https://images.pexels.com/photos/16883535/pexels-photo-16883535/free-photo-of-woman-in-white-dress-posing-in-building-door.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" className='w-full object-cover h-[200px] rounded-lg' />
+          </div>
+        </div>
+        <div className='flex flex-col gap-4'>
+          <div className='w-[450px] flex py-2 px-4 rounded-l-xl rounded-tr-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+            <h1 className='text-sm tracking-wider'>Introducing Txtly</h1>
+          </div>
+          <div className='w-[450px] flex py-2 px-4 rounded-l-xl rounded-tr-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+            <h1 className='text-sm tracking-wider'>A messenger style personal webpage.</h1>
+          </div>
+          <div className='flex items-end gap-2'>
+            <div className='w-[450px] flex py-2 px-4 rounded-l-xl rounded-tr-xl bg-gradient-to-r from-zinc-950 to-zinc-900 shadow shadow-zinc-800 backdrop-blur-xl border-[0.5px] border-gray-100/20'>
+              <h1 className='text-sm tracking-wider'>Supports all markdown syntax and also supports audio, video, spotify and more.</h1>
+            </div>
+            <img src="https://images.pexels.com/photos/5774802/pexels-photo-5774802.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" className='w-8 h-8 rounded-full object-cover' />
+          </div>
+        </div>
+      </section>
     </main>
   )
 }
